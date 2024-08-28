93bddb71a9da4dbab5b99df9496058f8563b1443
alitvak01 <alitvak01@saintmarys.edu>
2024-08-28 17:15:13 -0400

    Add files via upload

---
+++ Lab01.py
@@ -0,0 +1,45 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> "Hello World!"
+'Hello World!'
+>>> 2+2
+4
+>>> print ("Hello!")
+Hello!
+>>> 
+========= RESTART: C:/Users/aliza/OneDrive/Desktop/CompSci 207/lab1.py =========
+Welcome to CPSC 207!
+>>> #Lily DeYoung, Aliza Litvak
+>>> print ("Hello, world")
+Hello, world
+>>> print ("Welcome to CPSC 207")
+Welcome to CPSC 207
+>>> print ("Hello\nWorld")
+Hello
+World
+>>> print ("How\nare\nyou?")
+How
+are
+you?
+>>> print ("Today", "is", "Wednesday")
+Today is Wednesday
+>>> print ("TOday" + "is" + "Wednesday")
+TOdayisWednesday
+>>> 1+2*3
+7
+>>> (1+2)*3
+9
+>>> 7/3
+2.3333333333333335
+>>> 7//3
+2
+>>> 7%3
+1
+>>> 22//4
+5
+>>> 22%4
+2
+>>> 3**2
+9
+>>> 2**3
+8
